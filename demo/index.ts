d8d6c4577677c5becce964b9d8a9cfa13dc34dd4
Phap Duong Dieu <d2phap@gmail.com>
2021-05-09 20:25:40 +0800

    updated demo

--- demo/index.ts
+++ demo/index.ts
@@ -1,6 +1,11 @@
 
-import {user, author} from '@d2phap/npm-package-template';
+import { author, user } from '@d2phap/npm-package-template';
 
+// print out the values
+const el = document.getElementById('content');
+el.innerHTML = `NPM module data:<br/>
+<pre>author = <code>${JSON.stringify(author, null, 2)}</code></pre>
+<pre>user = <code>${JSON.stringify(user, null, 2)}</code></pre
+`;
 
-console.log(user, author);
-
+console.log(author, user);
